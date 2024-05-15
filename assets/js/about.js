d397a7275306ba235e0d742626c92ca344ae5d5b
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-05-15 21:11:33 +0100

    FEAT: Change layout of about page and add functionality

---
+++ assets/js/about.js
@@ -0,0 +1,26 @@
+let infoShown = false;
+
+window.onload = function() {
+  if (window.innerWidth < 576) {
+    window.addEventListener('click', infoOpen)
+  }
+}
+
+function infoOpen(event) {
+  if (infoShown === false) {
+    // Locate the corrosponding info to be shown
+    const elementId = event.target.id;
+    const elementNumber = elementId.split('-')[0];
+    const infoId = elementNumber + '-about';
+    infoElement = document.getElementById(infoId);
+    // Display the corrosponding info
+    infoElement.style.display = 'block';
+    infoShown = true;
+  } else {
+    const infoElements = document.getElementsByClassName('about-info');
+    for (const info of infoElements){
+      info.style.display = 'none';
+    }
+    infoShown = false;
+  }
+}
